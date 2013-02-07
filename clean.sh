bfe591c242282a393f7ea8b7ba069be1d798ae75
David Johnson <djohnson.m@gmail.com>
2013-02-07 11:51:45 -0600

    combined make scripts

--- clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-for f in *.hack; do echo "Removing .hack file: $f"; rm $f; done
-for f in *.pyc; do echo "Removing .pyc file: $f"; rm $f; done
--- make.sh
+++ make.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+for f in *.hack; do echo "Removing .hack file: $f"; rm $f; done
+for f in *.pyc; do echo "Removing .pyc file: $f"; rm $f; done
 for f in *.asm; do echo "Making .hack file for $f"; Assembler.py $f; done
 
